e1d592f6c6625e783bbdfc72996cadcf66369d42
javad-fallah-official <142024753+javad-fallah-official@users.noreply.github.com>
2025-04-01 19:58:26 +0330

    add urls of addProduct into the main urls File

--- WebStore/urls.py
+++ WebStore/urls.py
@@ -7,7 +7,6 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -15,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
+from addProduct import urls as addProduct_Urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('AddProduct/' ,include(addProduct_Urls))
 ]
