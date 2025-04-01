0fd81c2760cfa0b43dcdf2f392b6fdf5dbdbf0e7
javad-fallah-official <142024753+javad-fallah-official@users.noreply.github.com>
2025-04-01 20:21:18 +0330

    Create models of Products

--- addProduct/models.py
+++ addProduct/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+class Product(models.Model):
+    ProductID = models.IPAddressField(unique=True)
+    ProductName = models.CharField(unique=True)
+    Category = models.CharField()
+    Price = models.DecimalField()
+    Stock = models.IntegerField()
+    Brand = models.CharField()
+    Image = models.ImageField()
