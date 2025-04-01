6c2652cfacc755093239b949c6031799d59edd34
javad-fallah-official <142024753+javad-fallah-official@users.noreply.github.com>
2025-04-01 20:31:15 +0330

    make Migrations

--- addProduct/models.py
+++ addProduct/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 class Product(models.Model):
-    ProductID = models.IPAddressField(unique=True)
-    ProductName = models.CharField(unique=True)
-    Category = models.CharField()
-    Price = models.DecimalField()
+    ProductID = models.GenericIPAddressField(unique=True)
+    ProductName = models.CharField(unique=True ,max_length=150)
+    Category = models.CharField(max_length=50)
+    Price = models.DecimalField(max_digits=9 , decimal_places=2)
     Stock = models.IntegerField()
-    Brand = models.CharField()
+    Brand = models.CharField(max_length=50)
     Image = models.ImageField()
+    
