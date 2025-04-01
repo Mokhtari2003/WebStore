42caa899e62b16632b59841498ff116fd14e26c5
javad-fallah-official <142024753+javad-fallah-official@users.noreply.github.com>
2025-04-01 21:53:52 +0330

    fix migrations , add a test Record to db

--- addProduct/views.py
+++ addProduct/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .forms import PruductForm
 def addproduct(request):
-    ProductForm_V = PruductForm()
-    return render(request ,"addProduct/index.htm" , {'Pform' : ProductForm_V})
+    if request.method == 'POST':
+        ProductForm_V = PruductForm(request.POST ,request.FILES)
+        print(request.POST ,ProductForm_V.is_valid())
+        if ProductForm_V.is_valid():
+            ProductForm_V.save()
+            print("sucsessfully added")
+        else:
+            print(ProductForm_V.errors)
+    return render(request ,"addProduct/index.htm" , {'Pform' : PruductForm()})
